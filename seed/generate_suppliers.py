da058b2853c1017bc9799979d0cf4c16b0f93ef8
Adityacodin <aditya.exponentially3.14@gmail.com>
2026-06-06 23:09:37 +0530

    add strip_keywords function for email

--- seed/generate_suppliers.py
+++ seed/generate_suppliers.py
@@ -4,19 +4,23 @@ import db
 from random import choice
 from faker import Faker
 
+def strip_keywords(text):
+    return ''.join(char for char in text if char.isalnum()).lower()
+
 def main():
     conn = db.connect_db()
     cur = conn.cursor()
     faker = Faker('en_US')
     email_suffix = ['sales','hello','support','contact']
-    for i in range(0,10):
+    for i in range(20):
         company = faker.company()
+        email = choice(email_suffix)+"@"+strip_keywords(company)+".com"
         db.execute_query(
             cur,
         """INSERT INTO suppliers 
         (supplier_name,contact_email) VALUE 
         (?,?);
-        """,params = (company,choice(email_suffix)+"@"+company+".com")
+        """,params = (company,email)
         )
         db.save_changes(conn)
     db.close_db(conn)
